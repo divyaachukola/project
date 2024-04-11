fbc933a1b696a0d4b95dc25626d2ed0c8d66a811
divyaachukola <166644203+divyaachukola@users.noreply.github.com>
2024-04-12 00:16:28 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,20 +1,27 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for PGproject project.
 
-urlpatterns=[
-    path('first',views.firstpage,name='firstpageurl'),
-    path('stdlogin',views.studentlogin,name='studentloginurl'),
-    path('stdsignup',views.studentsignup,name='studentsignupurl'),
-    path('pglogin',views.pglogin,name='pgloginurl'),
-    path('pgsignup',views.pgsignup,name='pgsignupurl'),
-    path('pgform',views.pgform,name='pgformurl'),
-    path('pgdetails',views.pgdetails,name='pgdetailsurl'),
-    path('pgmain',views.pgmain,name='pgmainurl'),
-    path('pgshare',views.pgshare,name='pgshareurl'),
-    path('logout',views.user_logout,name='logouturl'),
-    path('pglogout',views.admin_logout,name='pglogouturl'),
-    path('complete/<int:eno>',views.complete,name='completeurl'),
-    path('registerpg/<int:eno>',views.registerpg,name='registerpgurl'),
-    path('stdlist/',views.stdlist,name='stdlisturl'),
-    path('delete/<int:eno>',views.deleteData , name='deleteurl')
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('firstpage/',include('firstpage.urls')),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
